66694fad89c8fc046387990cfb6c265ce2671993
NGreben <96392178+NGreben@users.noreply.github.com>
2023-03-12 15:15:38 +0300

    Add files via upload

---
+++ Program_13.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int number;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine()!, out number);
+
+if (number / 100 > 0)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    number = number % 10;
+    Console.Write($"Третья цифра числа: {number}");
+}
+else
+    Console.Write($"Третьей цифры нет");
